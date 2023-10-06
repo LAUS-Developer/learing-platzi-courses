7252242cc5ea2ae98381f5f5f5b6d12b98e4eee3
LAUS Developer <lausdeveloper@outlook.com>
2023-10-06 02:53:20 -0500

    pc-fdp-mokepon-03
    Added random enemies.

--- mokepon/js/mokepon.js
+++ mokepon/js/mokepon.js
@@ -1,34 +1,64 @@
-function gameStart(){
+function gameStart() {
     let btnPetPlayer = document.getElementById("btn-pet")
 
     btnPetPlayer.addEventListener("click", selectPetPlayer)
 
-/*
-    function selectPetPlayer(){        
-        alert("Pet Selected")
-    };
-*/
+    /*
+        function selectPetPlayer(){        
+            alert("Pet Selected")
+        };
+    */
 
 }
 
-function selectPetPlayer(){
+
+function selectPetPlayer() {
 
     let inputHipodoge = document.getElementById("Hipodoge")
     let inputCapipepo = document.getElementById("Capipepo")
     let inputRatclue = document.getElementById("Ratclue")
 
-    if (inputHipodoge.checked){
-        alert("Selected Hipodoge")
+    let spanPetPlayer = document.getElementById('pet-player')
+
+
+
+    if (inputHipodoge.checked) {
+        spanPetPlayer.innerHTML = "Hipodoge"
+
+        console.log(spanPetPlayer)
     }
-    else if (inputCapipepo.checked){
-        alert("Selected Capipepo")
+    else if (inputCapipepo.checked) {
+        spanPetPlayer.innerHTML = "Capipepo"
     }
-    else if (inputRatclue.checked){
-        alert("Selected Ratclue")
+    else if (inputRatclue.checked) {
+        spanPetPlayer.innerHTML = "Ratclue"
     } else {
-        alert("Escoge algo man")
+        //alert("Escoge algo man")
+    }
+
+    selectPetPc()
+
+
+}
+
+function selectPetPc() {
+    let randomAttack = random(1, 3)
+    let spanPetPc = document.getElementById("pet-pc")
+
+    if (randomAttack == 1) {
+        //Hipodoge
+        spanPetPc.innerHTML="Hipodoge"
+    } else if (randomAttack == 2) {
+        //Capipepo
+        spanPetPc.innerHTML="Capipepo"
+    } else if (randomAttack == 3){
+        //Raticlue
+        spanPetPc.innerHTML="Ratclue"
     }
 }
 
+function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-window.addEventListener("load",gameStart);
+window.addEventListener("load", gameStart);
