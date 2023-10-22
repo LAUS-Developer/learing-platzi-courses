44e9fdf580d45e5456e2ab5f9516f99ae878c144
LAUS Developer <lausdeveloper@outlook.com>
2023-10-22 14:43:06 -0500

    pc-fdp-mokepon-opt-js-01
    Organizing variables

--- mokepon_3/js/mokepon.js
+++ mokepon_3/js/mokepon.js
@@ -1,75 +1,65 @@
 let playerAttack;
 let pcAttack;
-let playerLifes = 3
-let pcLifes = 3
+const playerLifes = 3;
+const pcLifes = 3;
+
+const sectionSelectAttack = document.getElementById("select-attack");
+const btnPetPlayer = document.getElementById("btn-pet");
+
+const sectionReset = document.getElementById ("restart");
+
+const btnFire = document.getElementById("btn-fire");
+const btnWater = document.getElementById("btn-water");
+const btnEarth = document.getElementById("btn-earth");
+
+const sectionSelectPet = document.getElementById("select-pet");
+const inputHipodoge = document.getElementById("Hipodoge");
+const inputCapipepo = document.getElementById("Capipepo");
+const inputRatclue = document.getElementById("Ratclue");
+const spanPetPlayer = document.getElementById('pet-player');
+
+let randomPc = random(1, 3);
+const spanPetPc = document.getElementById("pet-pc");
+
+let randomAttack = random(1,3);
+
+const spanPlayerLifes = document.getElementById("playerLifes");
+const spanPcLifes = document.getElementById("pcLifes");
+
+const sectionParagraph = document.getElementById("combat-messages");
+const paragraph = document.createElement("p");  
+const sectionResult = document.getElementById('result')
+const sectionPlayerAttack = document.getElementById ('playerAttack')
+const sectionPcAttack = document.getElementById ("pcAttack")
+
+const newPlayerAttack = document.createElement('p')
+const newPcAttack = document.createElement('p')
+
+const sectionParagraphResult = document.getElementById("endResultMessage");
+const paragraphResult = document.createElement("p");
 
 function gameStart() {
-    let sectionSelectAttack = document.getElementById("select-attack");
     sectionSelectAttack.style.display="none";
-    
-    let btnPetPlayer = document.getElementById("btn-pet")
-    btnPetPlayer.addEventListener("click", selectPetPlayer)
-
-   let sectionReset = document.getElementById ("restart");
-    sectionReset.style.display="none"
-
-  
-
-    let btnFire = document.getElementById("btn-fire")
+    sectionReset.style.display="none";
+    btnPetPlayer.addEventListener("click", selectPetPlayer);
     btnFire.addEventListener("click", attackFire);
-    let btnWater = document.getElementById("btn-water")
     btnWater.addEventListener("click", attackWater);
-    let btnEarth = document.getElementById("btn-earth")
     btnEarth.addEventListener("click", attackEarth);
-
 };
 function selectPetPlayer() { 
-    
-    let sectionSelectPet = document.getElementById("select-pet")
     sectionSelectPet.style.display="none";
-
-    let sectionSelectAttack = document.getElementById("select-attack")
-    sectionSelectAttack.style.display="block";
-
-
-
-    
-    
-    let inputHipodoge = document.getElementById("Hipodoge")
-    let inputCapipepo = document.getElementById("Capipepo")
-    let inputRatclue = document.getElementById("Ratclue")
-
-    let spanPetPlayer = document.getElementById('pet-player')
-
-    if (inputHipodoge.checked) {
-        spanPetPlayer.innerHTML = "Hipodoge"
-    }
-    else if (inputCapipepo.checked) {
-        spanPetPlayer.innerHTML = "Capipepo"
-    }
-    else if (inputRatclue.checked) {
-        spanPetPlayer.innerHTML = "Ratclue"
-    } else {
-        alert("Escoge algo man")
-    }
+    sectionSelectAttack.style.display="block";     
+    if (inputHipodoge.checked){spanPetPlayer.innerHTML = "Hipodoge"}
+    else if (inputCapipepo.checked){spanPetPlayer.innerHTML = "Capipepo"}
+    else if (inputRatclue.checked){spanPetPlayer.innerHTML = "Ratclue"}
+    else{alert("Escoge algo man")}
     selectPetPc()
-
 };
 
 function selectPetPc() {
-    let randomPc = random(1, 3)
-    let spanPetPc = document.getElementById("pet-pc")
-
-    if (randomPc == 1) {
-        //Hipodoge
-        spanPetPc.innerHTML = "Hipodoge"
-    } else if (randomPc == 2) {
-        //Capipepo
-        spanPetPc.innerHTML = "Capipepo"
-    } else if (randomPc == 3) {
-        //Raticlue
-        spanPetPc.innerHTML = "Ratclue"
-    }
+    if(randomPc == 1){spanPetPc.innerHTML="Hipodoge"}
+    else if(randomPc == 2){spanPetPc.innerHTML="Capipepo"}
+    else if(randomPc == 3){spanPetPc.innerHTML="Ratclue"}
 };
 function attackFire() {
     playerAttack = "fire";
@@ -84,76 +74,29 @@ function attackWater() {
     randomAttackPc()
 };
 function randomAttackPc() {
-    let randomAttack = random(1,3);
-
-    if (randomAttack == 1) {
-        pcAttack = "fire";
-    } else if (randomAttack == 2) {
-        pcAttack = "earth";
-    } else {
-        pcAttack = "water";
-    }
+    if(randomAttack == 1){pcAttack = "fire";}
+    else if(randomAttack == 2) {pcAttack = "earth";}
+    else {pcAttack = "water";}
 
     combat()
-   
 };
 function combat(){
-    let spanPlayerLifes = document.getElementById("playerLifes")
-    let spanPcLifes     = document.getElementById("pcLifes")
-
-
-
     if (pcAttack == playerAttack){
         createMessage("Tie")
-    }else if (playerAttack == "fire" && pcAttack == "earth"){
-        createMessage("You Win!")
-        //triunfos = triunfos + 1
-
-        spanPcLifes.innerHTML = pcLifes
-        pcLifes--;
-    }else if (playerAttack == "water" && pcAttack == "fire"){
-        createMessage("You Win!")
-        //triunfos = triunfos + 1
-         spanPcLifes.innerHTML = pcLifes
-         pcLifes--;
-    }else if (playerAttack == "earth" && pcAttack == "water"){
-        createMessage ("You win!")
-        //triunfos = triunfos + 1
-         spanPcLifes.innerHTML = pcLifes
-         pcLifes--;
-    }else{
-        createMessage("LOSER")
-        //partidas = perdidas + 1
-
-        playerLifes--;
-        spanPlayerLifes.innerHTML=playerLifes;
-    }
+    }else if (playerAttack == "fire" && pcAttack == "earth"){createMessage("You Win!");spanPcLifes.innerHTML = pcLifes;pcLifes--;
+    }else if (playerAttack == "water" && pcAttack == "fire"){createMessage("You Win!");spanPcLifes.innerHTML = pcLifes;pcLifes--;
+    }else if (playerAttack == "earth" && pcAttack == "water"){createMessage ("You win!");spanPcLifes.innerHTML = pcLifes;pcLifes--;
+    }else{createMessage("LOSER");playerLifes--;spanPlayerLifes.innerHTML=playerLifes;}
 };
 
 function checklifes() {
-    if (pcLifes == 0) {
-        createEndMessage("Player Wins!")
-    } else if (playerLifes == 0) {
-        createEndMessage('PC Wins!')
-    }
+    if (pcLifes == 0) {createEndMessage("Player Wins!")} 
+    else if (playerLifes == 0){createEndMessage('PC Wins!')}
 }
 
 function createMessage(combatResult){
-
-    let sectionParagraph = document.getElementById("combat-messages");
-    let paragraph = document.createElement("p");    
     paragraph.innerHTML ="Your pet attacked with "+playerAttack+". Enemy pet attacked with "+ pcAttack + " " + combatResult;
     sectionParagraph.appendChild(paragraph);
-
-
-
-    let sectionResult = document.getElementById('result')
-    let sectionPlayerAttack = document.getElementById ('playerAttack')
-    let sectionPcAttack = document.getElementById ("pcAttack")
-
-    let newPlayerAttack = document.createElement('p')
-    let newPcAttack = document.createElement('p')
-
     sectionResult.innerHTML = combatResult
     newPlayerAttack.innerHTML = playerAttack
     newPcAttack.innerHTML = pcAttack
@@ -161,29 +104,22 @@ function createMessage(combatResult){
     sectionPlayerAttack.appendChild(newPlayerAttack)
     sectionPcAttack.appendChild(newPcAttack)
 
-
     checklifes()
 };
 
 
 function createEndMessage(combatEndResult){
-    let sectionParagraph = document.getElementById("endResultMessage");
-
-    let paragraph = document.createElement("p");
-    paragraph.setAttribute("id","combatEndResult")
-    paragraph.innerHTML = combatEndResult;
-
-    sectionParagraph.appendChild(paragraph);
 
 
-    let btnFire = document.getElementById("btn-fire")
+    paragraphResult.setAttribute("id","combatEndResult")
+    paragraphResult.innerHTML = combatEndResult;
+
+    sectionParagraphResult.appendChild(paragraphResult);
+
     btnFire.disabled=true
-    let btnWater = document.getElementById("btn-water")
     btnWater.disabled=true
-    let btnEarth = document.getElementById("btn-earth")
-    btnEarth.disabled=true
+    btnEarth.disabled=true   
     
-    let sectionReset = document.getElementById ("restart");
     sectionReset.style.display="block"
 
 }
